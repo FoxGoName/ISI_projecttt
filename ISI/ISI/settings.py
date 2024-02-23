dc6d0218ab734553faebf3160630eb513eb17d92
FoxGoName <106571781+FoxGoName@users.noreply.github.com>
2024-02-24 00:46:55 +0800

    detail contact

--- ISI/ISI/settings.py
+++ ISI/ISI/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.core',
+    'apps.store',
 ]
 
 
@@ -67,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'apps.store.context_processor.menu_categories',
             ],
         },
     },
