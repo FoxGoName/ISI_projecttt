838c6810a21ffb7fae5aeed4f3ae1a43f9d2212c
Nick-60 <2964186029@qq.com>
2024-02-24 17:56:11 +0800

    Odering products by time

--- ISI/apps/store/models.py
+++ ISI/apps/store/models.py
@@ -20,6 +20,10 @@ class Product(models.Model):
       description = models.TextField(blank=True, null=True)
       price = models.FloatField()
       id = models.AutoField(primary_key=True)
+      date_added = models.DateTimeField(auto_now_add=True)
+
+      class Meta:
+        ordering = ('-date_added',)
 
       def __str__(self):
           return self.title
